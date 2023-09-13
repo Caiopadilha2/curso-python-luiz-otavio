25be2cd574d22089617ffca5cd3babd5fee38d42
Caio Padilha <caiopadilha@id.uff.br>
2023-09-13 15:19:33 -0300

    Listas

---
+++ aula49.py
@@ -0,0 +1,24 @@
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis:
+    append, insert, pop, del, clear, extend, +
+Create Read Update   Delete
+Criar, ler, alterar, apagar = lista[i] (CRUD)
+"""
+
+lista = [10, 20, 30, 40]
+# print(lista)
+# lista[2] = 300
+# print(lista)
+# del lista[2]
+# print(lista)
+lista.append(50)
+lista.append(60)
+print(lista)
+lista.pop()
+print(lista)
+lista.pop(1)
+print(lista)
